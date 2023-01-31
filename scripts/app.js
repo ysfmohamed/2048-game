ec15170d9513c48b7fce359f57f5deadcadabcdf
Youssef Mohamed <yussf.mohamed.o@gmail.com>
2023-01-31 20:34:29 +0200

    add movements algorithms

--- scripts/app.js
+++ scripts/app.js
@@ -97,20 +97,126 @@ class Random {
   }
 }
 
+class Logic {
+  constructor() {}
+
+  static moveBlocksToLeft = (matrix) => {
+    let newMatrix = this.compress(matrix);
+    const matrixAfterMerge = this.merge(newMatrix);
+    newMatrix = this.compress(matrixAfterMerge);
+
+    return newMatrix;
+  };
+
+  static moveBlocksToRight = (matrix) => {
+    let newMatrix = this.reverse(matrix);
+    newMatrix = this.moveBlocksToLeft(newMatrix);
+    this.reverse(newMatrix);
+
+    return newMatrix;
+  };
+
+  static moveBlocksToUp = (matrix) => {
+    let newMatrix = this.transpose(matrix);
+    newMatrix = this.moveBlocksToLeft(newMatrix);
+    newMatrix = this.transpose(newMatrix);
+
+    return newMatrix;
+  };
+
+  static moveBlocksToDown = (matrix) => {
+    let newMatrix = this.transpose(matrix);
+    newMatrix = this.moveBlocksToRight(matrix);
+    newMatrix = this.transpose(newMatrix);
+
+    return newMatrix;
+  };
+
+  static compress = (matrix) => {
+    let pos = 0;
+
+    for (let i = 0; i < 4; i++) {
+      pos = 0;
+      for (let j = 0; j < 4; j++) {
+        if (matrix[i][j].innerHTML !== "") {
+          matrix[i][pos].innerHTML = matrix[i][j].innerHTML;
+          if (j !== pos) {
+            matrix[i][j].innerHTML = "";
+          }
+          pos += 1;
+        }
+      }
+      pos = 0;
+    }
+
+    return matrix;
+  };
+
+  static merge = (matrix) => {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (
+          matrix[i][j].innerText === matrix[i][j + 1].innerText &&
+          matrix[i][j].innerText !== ""
+        ) {
+          let currentValue = parseInt(matrix[i][j].innerText) * 2;
+          matrix[i][j].innerHTML = `<div>${currentValue}</div>`;
+          matrix[i][j + 1].innerHTML = "";
+        }
+      }
+    }
+
+    return matrix;
+  };
+
+  static reverse = (matrix) => {
+    let start = 0;
+    let end = 3;
+    for (let i = 0; i < 4; i++) {
+      start = 0;
+      end = 3;
+      for (; start <= end; ) {
+        let temp = matrix[i][start].innerHTML;
+        matrix[i][start].innerHTML = matrix[i][end].innerHTML;
+        matrix[i][end].innerHTML = temp;
+        start += 1;
+        end -= 1;
+      }
+    }
+
+    return matrix;
+  };
+
+  static transpose = (matrix) => {
+    for (let i = 0; i < 4; i++) {
+      for (let j = i + 1; j < 4; j++) {
+        let temp = matrix[i][j].innerHTML;
+        matrix[i][j].innerHTML = matrix[j][i].innerHTML;
+        matrix[j][i].innerHTML = temp;
+      }
+    }
+
+    return matrix;
+  };
+}
+
 window.addEventListener("keydown", (event) => {
+  const board = game.getBoard().getMatrix();
   switch (event.key) {
     case "ArrowUp":
-      console.log("Up");
+      Logic.moveBlocksToUp(board);
       break;
     case "ArrowDown":
       console.log("Down");
+      Logic.moveBlocksToDown(board);
       break;
     case "ArrowRight":
       console.log("Right");
-      UserEvent.shiftBlocksToRight(game.getBoard().getMatrix());
+      Logic.moveBlocksToRight(board);
       break;
     case "ArrowLeft":
       console.log("Left");
+      Logic.moveBlocksToLeft(board);
       break;
   }
 });
@@ -124,4 +230,3 @@ newGameButton.addEventListener("click", () => {
 
 /* ########## START GAME ########## */
 let game = new Game();
-console.log(game.getBoard().getMatrix());
